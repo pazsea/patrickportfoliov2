218b9a435593dfbdc7abd4086920004bd5bf12fa
paz198787 <patrick.sjoberg@student.kyh.se>
2019-01-04 02:32:07 +0100

    working on navbar

--- src/components/js/Navbar.js
+++ src/components/js/Navbar.js
@@ -8,8 +8,8 @@ class Nav extends Component {
     return (
       <Sticky>
         <nav>
-          <ul>
-            <li>
+          <h1 className="logo">
+            <a>
               <Link
                 activeClass="active"
                 to="welcome"
@@ -21,46 +21,55 @@ class Nav extends Component {
               >
                 Home
               </Link>
+            </a>
+          </h1>
+
+          <ul className="main-nav">
+            <li>
+              <a>
+                <Link
+                  activeClass="active"
+                  to="aboutme"
+                  spy={true}
+                  smooth={true}
+                  offset={-65}
+                  duration={500}
+                  onSetActive={this.handleSetActive}
+                >
+                  About Me
+                </Link>
+              </a>
+            </li>
+            <li>
+              <a>
+                <Link
+                  activeClass="active"
+                  to="portfolio"
+                  spy={true}
+                  smooth={true}
+                  offset={-65}
+                  duration={500}
+                  onSetActive={this.handleSetActive}
+                >
+                  Portfolio
+                </Link>
+              </a>
             </li>
 
             <li>
-              <Link
-                activeClass="active"
-                to="aboutme"
-                spy={true}
-                smooth={true}
-                offset={-38}
-                duration={500}
-                onSetActive={this.handleSetActive}
-              >
-                About Me
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="active"
-                to="portfolio"
-                spy={true}
-                smooth={true}
-                offset={-38}
-                duration={500}
-                onSetActive={this.handleSetActive}
-              >
-                Portfolio
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="active"
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-                onSetActive={this.handleSetActive}
-              >
-                Contact
-              </Link>
+              <a>
+                <Link
+                  activeClass="active"
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                  onSetActive={this.handleSetActive}
+                >
+                  Contact
+                </Link>
+              </a>
             </li>
           </ul>
         </nav>
