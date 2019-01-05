de1f78e142719901ef75f772fa7d84a04cdfb8b1
paz198787 <patrick.sjoberg@student.kyh.se>
2019-01-05 15:16:48 +0100

    working on sidebar styld buttons

--- src/components/js/SideDrawer.js
+++ src/components/js/SideDrawer.js
@@ -1,30 +1,64 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-scroll";
 
-import '../css/sideDrawer.css';
+import "../css/sideDrawer.css";
 
 const sideDrawer = props => {
   return (
     <nav className="side-drawer">
-    
       <ul>
-      <li><button className="close" onClick={props.click}><i class="fas fa-times-circle fa-3x"></i></button> </li>
         <li>
-          <a href="/">Products</a>
+          <button className="close" onClick={props.click}>
+            <i class="fas fa-times-circle fa-3x" />
+          </button>{" "}
         </li>
         <li>
-          <a href="/">Users</a>
+          <Link
+            activeClass="active"
+            to="welcome"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            <a>Home</a>
+          </Link>
         </li>
         <li>
-          <a href="/">Users</a>
+          <Link
+            activeClass="active"
+            to="aboutme"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            <a>About Me</a>
+          </Link>
         </li>
         <li>
-          <a href="/">Users</a>
+          <Link
+            activeClass="active"
+            to="portfolio"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            <a>Portfolio</a>
+          </Link>
         </li>
         <li>
-          <a href="/">Users</a>
-        </li>
-        <li>
-          <a href="/">Users</a>
+          <Link
+            activeClass="active"
+            to="Feedback"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            <a>Feedback</a>
+          </Link>
         </li>
       </ul>
     </nav>
