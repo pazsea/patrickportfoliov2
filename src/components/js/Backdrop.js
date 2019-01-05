75e86270d168d2ac64f268558694e4a306211bd3
paz198787 <patrick.sjoberg@student.kyh.se>
2019-01-05 15:05:08 +0100

    fix style on backdrop and sidebar

--- src/components/js/Backdrop.js
+++ src/components/js/Backdrop.js
@@ -3,7 +3,7 @@ import React from "react";
 import "../css/backdrop.css";
 
 const Backdrop = props => (
-  <button id="backdrop" onClick={props.click}><i class="fas fa-times-circle fa-5x"></i></button>
+  <button id="backdrop" onClick={props.click}></button>
 );
  
 
--- src/components/js/SideDrawer.js
+++ src/components/js/SideDrawer.js
@@ -5,10 +5,23 @@ import '../css/sideDrawer.css';
 const sideDrawer = props => {
   return (
     <nav className="side-drawer">
-    <button className="close" onClick={props.click}><i class="fas fa-times-circle fa-3x"></i></button> 
+    
       <ul>
+      <li><button className="close" onClick={props.click}><i class="fas fa-times-circle fa-3x"></i></button> </li>
         <li>
           <a href="/">Products</a>
+        </li>
+        <li>
+          <a href="/">Users</a>
+        </li>
+        <li>
+          <a href="/">Users</a>
+        </li>
+        <li>
+          <a href="/">Users</a>
+        </li>
+        <li>
+          <a href="/">Users</a>
         </li>
         <li>
           <a href="/">Users</a>
