81118b7f923fbc3c6245416a6d7c7af3fc47bdb0
paz198787 <patrick.sjoberg@student.kyh.se>
2019-01-10 14:19:47 +0100

    api fixed need to apply correct url

--- src/components/js/Cards.js
+++ src/components/js/Cards.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Cards = props => {
+const API =
+  "https://api.github.com/repos/pazsea/patrickportfoliov2/stats/contributors";
+class Cards extends Component {
+  state = {   };
 
+  componentDidMount() {
+    fetch(this.props.api_url)
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({commit: json[0].total})
 
-  return (
-    <div className="container-all">
-      <img src={props.url} alt={props.alt} />
-      <h3>{props.head}</h3>
-      <p style={{ padding: "20px"}}>{props.text}</p>
-      <button className="readmore">Read More...</button>
-    </div>
-  );
-};
+      }); 
+  }
+
+  render() {
+    return (
+      <div className="container-all">
+        <img src={this.props.url} alt={this.props.alt} />
+        <h3>{this.props.head}</h3>
+        <p style={{ padding: "20px" }}>{this.props.text}</p>
+        <p>{this.state.commit} My</p>
+        <button className="readmore">Read More...</button>
+      </div>
+    );
+  }
+}
 
 export default Cards;
--- src/index.js
+++ src/index.js
@@ -6,27 +6,31 @@ import * as serviceWorker from "./serviceWorker";
 
 
 
+
 const projects = [
   {
     id: "1",
     head: "My first Landing Page",
     url: require("../src/components/pics/firstproject.png"),
     alt: "ett",
-    text: "Ok, this is where I was 6 months ago. I created my first landing page and I was so excited about the hover effect over the button. You got to start somewhere."
+    text: "Ok, this is where I was 6 months ago. I created my first landing page and I was so excited about the hover effect over the button. You got to start somewhere.",
+    api_url: "https://api.github.com/repos/pazsea/patrickportfoliov2/stats/contributors"
   },
   {
     id: "2",
     head: "My first portfolio website",
     url: require("../src/components/pics/myportfolio.png"),
     alt: "två",
-    text: "After two monthts in school we got an big assignment. We created an portfolio page. Mine was done only in CSS, HTML and JS."
+    text: "After two monthts in school we got an big assignment. We created an portfolio page. Mine was done only in CSS, HTML and JS.",
+    api_url: "https://api.github.com/repos/pazsea/patrickportfoliov2/stats/contributors"
   },
   {
     id: "3",
     head: "Our text editor",
     url: require("../src/components/pics/quirepage.png"),
     alt: "tre",
-    text: "This was our first group project. For this we had instruction to creat an fully fuctional text editor where the user could save, retrieve and marke notes in LS."
+    text: "This was our first group project. For this we had instruction to creat an fully fuctional text editor where the user could save, retrieve and marke notes in LS.",
+    api_url: "https://api.github.com/repos/pazsea/patrickportfoliov2/stats/contributors"
   },
 ];
 
